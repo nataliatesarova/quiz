198bec35444d3047e7351ebc4d10269aae3c04c6
nataliatesarova <naftalintesarova@gmail.com>
2023-02-19 16:56:59 +0000

    Add questions in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+let questions = [
+
+    {
+        question: "What is the capital city of Japan?",
+        answers: {
+            Option1: "Tokyo",
+            Option2: "Osaka",
+            Option3: "Kyoto",
+            Option4: "London",
+        },
+        rightAnswer: "1"
+    },
+
+    {
+        question: "What is the capital city of Estonia?",
+        answers: {
+            Option1: "Tartu",
+            Option2: "Riga",
+            Option3: "Tallinn",
+            Option4: "Vilnius",
+        },
+        rightAnswer: "3"
+    },
+
+    {
+        question: "What is the capital city of Brazil?",
+        answers: {
+            Option1: "São Paulo",
+            Option2: "Rio de Janeiro",
+            Option3: "Brasília",
+            Option4: "Buenos Aires",
+        },
+        rightAnswer: "3"
+    },
+
+    {
+        question: "What is the capital city of Israel?",
+        answers: {
+            Option1: "Cairo",
+            Option2: "Damascus",
+            Option3: "Tel Aviv-Yafo",
+            Option4: "Jerusalem",
+        },
+        rightAnswer: "4"
+    },
+
+    {
+        question: "What is the capital city of Lebanon?",
+        answers: {
+            Option1: "Tripoli",
+            Option2: "Beirut",
+            Option3: "Ankara",
+            Option4: "Baalbek",
+        },
+        rightAnswer: "2"
+    },
+
+    {
+        question: "What is the capital city of The United States of America?",
+        answers: {
+            Option1: "Los Angeles",
+            Option2: "New York City",
+            Option3: "Washington, D.C",
+            Option4: "Chicago",
+        },
+        rightAnswer: "3"
+    },
+
+    {
+        question: "What is the capital city of Jamaica?",
+        answers: {
+            Option1: "Hamilton",
+            Option2: "Havana",
+            Option3: "George Town",
+            Option4: "Kingston",
+        },
+        rightAnswer: "4"
+    },
+
+    {
+        question: "What is the capital city of Greece?",
+        answers: {
+            Option1: "Patras",
+            Option2: "Athens",
+            Option3: "Larissa",
+            Option4: "Thessaloniki",
+        },
+        rightAnswer: "2"
+    },
+
+    {
+        question: "What is the capital city of Spain?",
+        answers: {
+            Option1: "Madrid",
+            Option2: "Barcelona",
+            Option3: "Seville",
+            Option4: "Valencia",
+        },
+        rightAnswer: "1"
+    },
+
+    {
+        question: "What is the capital city of India?",
+        answers: {
+            Option1: "Kolkata",
+            Option2: "Mumbai",
+            Option3: "Bangalore",
+            Option4: "New Delhi",
+        },
+        rightAnswer: "4"
+    },
+];
+console.log(questions[9].rightAnswer);
